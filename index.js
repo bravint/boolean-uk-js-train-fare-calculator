d7c8d7c9a01eaab061d0e54ad01208c45e83fae5
Bravin Thillainathan <bravint@gmail.com>
2021-10-19 16:22:15 +0100

    js: round down ticket cost to 2dp

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const age = prompt(`What old are you?`);
 const distance = prompt(`How far are you travelling today in km?`)
+const unitPrice = 0.21;
+const under18Discount = 0.2;
+const over65Discount = 0.4;
 
 let ticketCost = 0
 
 if (age < 18) {
-    ticketCost = distance*(0.21-(0.21*0.2))
+    ticketCost = distance*(unitPrice-(unitPrice*under18Discount))
 } else if (age > 65) {
-    ticketCost = distance*(0.21-(0.21*0.4))
+    ticketCost = distance*(unitPrice-(unitPrice*over65Discount))
 } else {
     ticketCost = distance*0.21
 };
+
+ticketCost = ticketCost.toFixed(2)
 
 alert(`You fare will cost you £`+ticketCost);
