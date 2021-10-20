565e01af55f44a86653d43b8bc9330b3110ee8b9
Bravin Thillainathan <bravint@gmail.com>
2021-10-20 01:30:19 +0100

    html/js: used html form instead of prompts

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const age = prompt(`What old are you?`);
-/*const distance = prompt(`How far are you travelling today in km?`); ONLY APPLIES TO ORIGINAL EXERCISE*/
-const destination = prompt('Choose a destination 1  2  3 or 4');
-const unitPrice = 0.21;
+const unitCost = 0.21;
 const under18Discount = 0.2;
 const over65Discount = 0.4;
-
-let ticketCost = 0
+let ticketCost = 0;
 let distance = 0;
 
-if (destination == 1) {
-    distance = 100
-} else if (destination == 2) {
-    distance = 200
-} else if (destination == 3) {
-    distance = 300
-} else if (destination == 4) {
-    distance = 400
-};
+function ticket() {
+    const age = document.getElementById("age").value;
+    const destination = document.getElementById("destination").value;
+    console.log (age);
+    console.log (destination);
 
-if (age < 18) {
-    ticketCost = distance*(unitPrice-(unitPrice*under18Discount))
-} else if (age > 65) {18
-    ticketCost = distance*(unitPrice-(unitPrice*over65Discount))
-} else {
-    ticketCost = distance*0.21
-};
+    if (destination === "A") {
+        distance = 100
+    } else if (destination === "B") {
+        distance = 200
+    } else if (destination == "C") {
+        distance = 300
+    } else if (destination == "D") {
+        distance = 400
+    };
+    console.log (distance);
 
-ticketCost = ticketCost.toFixed(2)
+    if (age < 18) {
+        ticketCost = distance*(unitCost-(unitCost*under18Discount))
+    } else if (age > 65) {18
+        ticketCost = distance*(unitCost-(unitCost*over65Discount))
+    } else {
+        ticketCost = distance*0.21
+    };
 
-alert(`You fare will cost you £`+ticketCost);
-
-console.log (ticketCost);
-console.log (age);
-console.log (distance);
+    ticketCost = ticketCost.toFixed(2)
+    alert(`You fare will cost you £`+ticketCost);
+    console.log (ticketCost);
+}
